aba52f34c5f1df88ff582793af3c34b52b1c4637
Kevin Chatham <40923272+kevinchatham@users.noreply.github.com>
2022-04-25 11:52:23 -0500

    cleanup unused properties

--- Server/Models/Dto/TodoDto.cs
+++ Server/Models/Dto/TodoDto.cs
@@ -3,8 +3,6 @@ namespace Server.Models.Dto;
 public sealed class TodoDto
 {
     public string Id { get; set; }
-    public string CreatedIso { get; set; }
-    public string CompletedIso { get; set; }
     public string Value { get; set; }
     public bool Completed { get; set; }
 }
